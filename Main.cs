b0e21e05fcfd3d8e717cd1475d791a0562d76fca
Benjamin Nguyen <xccr123@gmail.com>
2025-02-13 10:51:21 -0800

    1.0.1

--- Main.cs
+++ Main.cs
@@ -1,11 +1,5 @@
 ﻿using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using GHPC.AI;
-using GHPC.Camera;
 using GHPC.Player;
 using GHPC.State;
 using GHPC.Vehicle;
@@ -13,11 +7,11 @@ using ManualLoading;
 using MelonLoader;
 using UnityEngine;
 using GHPC.Weapons;
-using System.Runtime.Remoting.Channels;
 using HarmonyLib;
 using GHPC.UI.Hud;
+using System.Linq;
 
-[assembly: MelonInfo(typeof(ManualLoadingMod), "Manual Loading", "1.0.0", "ATLAS")]
+[assembly: MelonInfo(typeof(ManualLoadingMod), "Manual Loading", "1.0.1", "ATLAS")]
 [assembly: MelonGame("Radian Simulations LLC", "GHPC")]
 
 namespace ManualLoading
@@ -27,6 +21,24 @@ namespace ManualLoading
         public static GameObject[] vic_gos;
         static MelonPreferences_Entry<bool> human_loader;
         static MelonPreferences_Entry<bool> auto_loader;
+
+        public class ManualLoadingHandler : MonoBehaviour
+        {
+            public AmmoFeed feed;
+            public static PlayerInput player_manager;
+
+            void Awake()
+            {
+                if (player_manager != null) return;
+                player_manager = GameObject.Find("_APP_GHPC_").GetComponent<PlayerInput>();
+            }
+
+            void Update()
+            {
+                feed.AutoReload = player_manager.CurrentPlayerUnit.gameObject.GetInstanceID() != gameObject.GetInstanceID();
+            }
+        }
+
 
         public IEnumerator GetVics(GameState _)
         {
@@ -51,14 +63,18 @@ namespace ManualLoading
                 if (vic == null) continue;
 
                 if (vic_go.GetComponent<Util.AlreadyConverted>() != null) continue;
-
-                vic_go.AddComponent<Util.AlreadyConverted>();
-
+                if (vic.WeaponsManager == null || vic.WeaponsManager.Weapons.Length == 0) continue;
                 WeaponSystem main_gun = vic.WeaponsManager.Weapons[0].Weapon;
+                AmmoFeed feed = main_gun.Feed;
 
-                if ((main_gun.Feed.HumanLoaded && human_loader.Value) || (main_gun.Feed.Autoloader != null && main_gun.Feed.LoadedClipType.Capacity == 1 && auto_loader.Value))
+                if 
+                (
+                    ((feed.HumanLoaded && human_loader.Value) || (feed.Autoloader != null && auto_loader.Value)) 
+                    && feed.LoadedClipType.Capacity == 1)
                 {
-                    main_gun.Feed.AutoReload = false;
+                    vic_go.AddComponent<Util.AlreadyConverted>();
+                    ManualLoadingHandler mlh = vic_go.AddComponent<ManualLoadingHandler>();
+                    mlh.feed = feed;
                 }
             }
 
@@ -67,17 +83,17 @@ namespace ManualLoading
 
         public override void OnSceneWasLoaded(int idx, string scene_name)
         {
-            if (scene_name == "MainMenu2_Scene" || scene_name == "LOADER_MENU" || scene_name == "LOADER_INITIAL" || scene_name == "t64_menu") return;
+            if (Util.menu_screens.Contains(scene_name)) return;
 
             StateController.RunOrDefer(GameState.GameReady, new GameStateEventHandler(GetVics), GameStatePriority.Medium);
             StateController.RunOrDefer(GameState.GameReady, new GameStateEventHandler(Convert), GameStatePriority.Medium);
         }
 
 
-        [HarmonyPatch(typeof(AlertHud), "AddAlertMessage")]
-        public static class ReplaceSound
-        {
-            public static bool Prefix(AlertHud __instance, object[] __args)
+        [HarmonyPatch(typeof(VehicleHudText), "AddAlertMessage")]
+        public static class SuppressNextAmmo
+        { 
+            public static bool Prefix(VehicleHudText __instance, object[] __args)
             {
                 if ((String)__args[0] == "Select next ammunition with 1, 2, 3, 4 or use Q to restock" || 
                     (String)__args[0] == "Select next ammunition with 1, 2, 3, 4") 
